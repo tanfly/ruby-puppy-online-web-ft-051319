d7d35d84f40d724902e635d606959612a73b5a6d
Stella McEwen <mcewen.stella@gmail.com>
2019-05-23 00:05:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  @@all = []
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+   puts @@all
+  end
+  
+  def self.clear_all 
+    @@all.clear
+  end
+end 
